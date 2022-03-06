d1998b2f0bec7f9673f48f98efffa7e760dafa1c
MadhuMallipudi <61326706+MadhuMallipudi@users.noreply.github.com>
2022-03-06 13:00:37 +0530

    city weather app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import  Home  from './Components/Home'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} exact />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
